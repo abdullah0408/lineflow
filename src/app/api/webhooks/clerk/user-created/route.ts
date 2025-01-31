44d855f28038908bd216d4a1499af80018c7ef46
abdullah <abdullah981066@gmail.com>
2025-01-31 20:58:02 +0530

    setup clerk webhook for user-created

---
+++ src/app/api/webhooks/clerk/user-created/route.ts
@@ -0,0 +1,70 @@
+import { Webhook } from "svix";
+import { headers } from "next/headers";
+import { UserJSON, WebhookEvent } from "@clerk/nextjs/server";
+import prisma from "@/lib/prisma";
+
+export async function POST(req: Request) {
+  const SIGNING_SECRET = process.env.SIGNING_SECRET;
+
+  if (!SIGNING_SECRET) {
+    throw new Error(
+      "Error: Please add SIGNING_SECRET from Clerk Dashboard to .env or .env.local"
+    );
+  }
+
+  // Create new Svix instance with secret
+  const wh = new Webhook(SIGNING_SECRET);
+
+  // Get headers
+  const headerPayload = await headers();
+  const svix_id = headerPayload.get("svix-id");
+  const svix_timestamp = headerPayload.get("svix-timestamp");
+  const svix_signature = headerPayload.get("svix-signature");
+
+  // If there are no headers, error out
+  if (!svix_id || !svix_timestamp || !svix_signature) {
+    return new Response("Error: Missing Svix headers", {
+      status: 400,
+    });
+  }
+
+  // Get body
+  const payload = await req.json();
+  const body = JSON.stringify(payload);
+
+  let evt: WebhookEvent;
+
+  // Verify payload with headers
+  try {
+    evt = wh.verify(body, {
+      "svix-id": svix_id,
+      "svix-timestamp": svix_timestamp,
+      "svix-signature": svix_signature,
+    }) as WebhookEvent;
+  } catch (err) {
+    console.error("Error: Could not verify webhook:", err);
+    return new Response("Error: Verification error", {
+      status: 400,
+    });
+  }
+
+  const userData = evt.data as UserJSON;
+
+  const id = userData.id;
+  const email = userData.email_addresses[0].email_address;
+  const firstName = userData.first_name || "";
+  const lastName = userData.last_name || "";
+  const name = `${firstName} ${lastName}`.trim() || email;
+
+  if (evt.type === "user.created") {
+    await prisma.user.create({
+      data: {
+        id,
+        email,
+        name,
+      },
+    });
+  }
+
+  return new Response("Webhook received", { status: 200 });
+}
