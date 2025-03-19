81aa91e2e6eadd48e4fab0950749e781a0dbd66f
abdullah <abdullah981066@gmail.com>
2025-03-19 20:43:43 +0530

    testing

--- src/app/(main)/dashboard/page.tsx
+++ src/app/(main)/dashboard/page.tsx
@@ -1,23 +1,9 @@
-// "use client";
-// import { useAuth } from "@/hooks/use-auth";
-// const Profile = () => {
-//   const user = useAuth();
+import React from 'react'
 
-//   console.log(user);
+const page = () => {
+  return (
+    <div>page</div>
+  )
+}
 
-//   return (
-//     <div>
-//       {user ? (
-//         <div>
-//           <h2>Hello World</h2>
-//           <h2>{user.userDetails?.clerkId}</h2>
-//           <p>{user.userDetails?.email}</p>
-//         </div>
-//       ) : (
-//         <p>Loading...</p>
-//       )}
-//     </div>
-//   );
-// };
-
-// export default Profile;
+export default page
--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,14 +1,14 @@
 // import { AuthProvider } from "@/context/AuthContext";
-// export default function MainLayout({
-//   children,
-// }: Readonly<{
-//   children: React.ReactNode;
-// }>) {
-//   return (
-//     <AuthProvider>
-//       <div className="h-screen w-screen m-0 p-0 flex flex-col">
-//           {children}
-//       </div>
-//     </AuthProvider>
-//   );
-// }
+export default function MainLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    // <AuthProvider>
+      <div className="h-screen w-screen m-0 p-0 flex flex-col">
+          {children}
+      </div>
+    // </AuthProvider>
+  );
+}
